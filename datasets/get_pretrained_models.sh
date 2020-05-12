6824f4345b2998500fbacd0f4e30f67f8e3da7b8
Yihui He 何宜晖 <yihuihe@foxmail.com>
2020-05-12 10:06:46 -0700

    fix typo
    resolve #1

--- datasets/get_pretrained_models.sh
+++ datasets/get_pretrained_models.sh
@@ -1,4 +1,4 @@
-wget https://github.com/yihui-he/epipolar-transformer/releases/download/outputs/resnet50-19c8e357.pth
-# wget https://github.com/yihui-he/epipolar-transformer/releases/download/outputs/resnet152-b121ed2d.pth
-wget https://github.com/yihui-he/epipolar-transformer/releases/download/outputs/pose_resnet_4.5_pixels_human36m.pth
-wget https://github.com/yihui-he/epipolar-transformer/releases/download/outputs/pairwise.pkl
+wget https://github.com/yihui-he/epipolar-transformers/releases/download/outputs/resnet50-19c8e357.pth
+# wget https://github.com/yihui-he/epipolar-transformers/releases/download/outputs/resnet152-b121ed2d.pth
+wget https://github.com/yihui-he/epipolar-transformers/releases/download/outputs/pose_resnet_4.5_pixels_human36m.pth
+wget https://github.com/yihui-he/epipolar-transformers/releases/download/outputs/pairwise.pkl
